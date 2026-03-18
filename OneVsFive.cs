a837660375edbc02b712b4d03abcc468493096a5
Dylan Radelet <dylanradelet@hotmail.com>
2026-03-18 21:15:26 +0100

    Ajout

--- OneVsFive.cs
+++ OneVsFive.cs
@@ -404,7 +404,7 @@ public class OneVsFivePlugin : BasePlugin
                 var pawn = ct.PlayerPawn.Value;
                 if (pawn == null || !ct.PawnIsAlive) continue;
 
-                pawn.Glow.GlowColorOverride = Color.Red;                    
+                pawn.Glow.GlowColorOverride = System.Drawing.Color.FromArgb(255, 255, 0, 0);                  
                 pawn.Glow.GlowType          = 3;      // 3 = visible through walls
                 pawn.Glow.GlowRange         = 9999;
                 pawn.Glow.GlowRangeMin      = 0;
